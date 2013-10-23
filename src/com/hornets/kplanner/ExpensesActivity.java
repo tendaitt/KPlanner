02caafc17925f2aa2ab2d15b9b9b67c703720542
Mehmet Kologlu <mehmetk@Mehmets-MacBook-Pro.local>
2013-10-23 16:46:05 -0400

    v1.0 of expenses activity/ added test button to homepage(planner activity) / fixed the naming issue

---
+++ src/com/hornets/kplanner/ExpensesActivity.java
@@ -0,0 +1,27 @@
+package com.hornets.kplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.DatePicker;
+
+public class ExpensesActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_expenses);
+
+		//get rid of the calendar view of the DatePicker
+		DatePicker dp = (DatePicker) findViewById(R.id.expenses_datePicker);
+		dp.setCalendarViewShown(false);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.expenses, menu);
+		return true;
+	}
+
+}
--- src/com/hornets/kplanner/PlannerActivity.java
+++ src/com/hornets/kplanner/PlannerActivity.java
@@ -1,10 +1,12 @@
 package com.hornets.kplanner;
 
-import com.hornets.kplannder.R;
+import com.hornets.kplanner.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class PlannerActivity extends Activity {
 
@@ -14,12 +16,20 @@ public class PlannerActivity extends Activity {
         setContentView(R.layout.activity_planner);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.planner, menu);
         return true;
     }
+    
+	/*
+	 * method for mehmet's test button to call for test purposes
+	 */
+	public void testMehmet(View view)
+	{
+		Intent intent = new Intent(this, ExpensesActivity.class);
+		startActivity(intent);
+	}
     
 }
