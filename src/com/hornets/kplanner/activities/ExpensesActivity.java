b47a7e55bccf375bace453f70cc39c5d3f550962
Mehmet Kologlu <mehmetk@Mehmets-MacBook-Pro.local>
2013-10-28 16:35:28 -0400

    got the date picker working, now the pick a date button displays the selected date

--- src/com/hornets/kplanner/activities/ExpensesActivity.java
+++ src/com/hornets/kplanner/activities/ExpensesActivity.java
@@ -1,5 +1,7 @@
 package com.hornets.kplanner.activities;
 
+import java.util.Calendar;
+
 import com.hornets.kplanner.R;
 import com.hornets.kplanner.fragments.DatePickerFragment;
 
@@ -18,12 +20,30 @@ import android.widget.Button;
  *
  */
 
-public class ExpensesActivity extends FragmentActivity 
-implements DatePickerFragment.DatePickerListener{
+public class ExpensesActivity extends FragmentActivity
+implements DatePickerFragment.DatePickerDialogListener{
+	
+	Button dateBtn;
+	int year;
+	int month;
+	int day;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_expenses);
+		
+		//get current date
+		final Calendar c = Calendar.getInstance();
+		year = c.get(Calendar.YEAR);
+		month = c.get(Calendar.MONTH);
+		day = c.get(Calendar.DAY_OF_MONTH);
+		
+		//define the date button
+		dateBtn = (Button) this.findViewById(R.id.expenses_button_datepicker);
+
+		//update the date picker button to display the date which is the current date
+		updateDateButtonText();
 	}
 
 	@Override
@@ -32,16 +52,34 @@ implements DatePickerFragment.DatePickerListener{
 		getMenuInflater().inflate(R.menu.expenses, menu);
 		return true;
 	}
-	
+
+	/*
+	 * onclick function of the pick a date button, 
+	 * displays the datepicker fragment
+	 */
 	public void showDatePickerDialog(View v){
 		DatePickerFragment datePickerFragment = new DatePickerFragment();
 		datePickerFragment.show(getSupportFragmentManager(), "datePicker");
 	}
 
+	/*
+	 * updates the text of the pick a date button
+	 */
+	public void updateDateButtonText()
+	{
+		dateBtn.setText(month + "/" + day + "/" + year);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.hornets.kplanner.fragments.DatePickerFragment.DatePickerDialogListener#onDateSelect(android.support.v4.app.DialogFragment, int, int, int)
+	 * 
+	 */
 	@Override
-	public void onDateSet(DialogFragment dialog, int year, int month, int day) {
-		// TODO Auto-generated method stub
-		Button pickADateBtn = (Button) findViewById(R.id.expenses_button_datepicker);
-		pickADateBtn.setText(month + "/" + day + "/" + year);
+	public void onDateSelect(DialogFragment dialog, int y, int monthOfYear, int dayOfMonth) {
+		year = y;
+		month = monthOfYear;
+		day = dayOfMonth;
+		updateDateButtonText();
 	}
 }
--- src/com/hornets/kplanner/fragments/DatePickerFragment.java
+++ src/com/hornets/kplanner/fragments/DatePickerFragment.java
@@ -6,16 +6,20 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.widget.Button;
 import android.widget.DatePicker;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
-	
-	public interface DatePickerListener{
-		public void onDateSet(DialogFragment dialog, int year, int month, int day);
+
+	public interface DatePickerDialogListener{
+		/*
+		 * called when the user sets a date.
+		 * is defined in ExpensesActivity
+		 */
+		public void onDateSelect(DialogFragment dialog, 
+				int year, int monthOfYear, int dayOfMonth);
 	}
-	
-	DatePickerListener mListener;
+
+	DatePickerDialogListener mListener;
 
 	@Override
 	public void onAttach(Activity activity)
@@ -24,15 +28,15 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 		//Verify that the host activity implements the callback interface
 		try{
 			//Instantiate the EnterFileNameDialogListener so we can send events to the host
-			mListener = (DatePickerListener) activity;
+			mListener = (DatePickerDialogListener) activity;
 		}
 		catch(ClassCastException e) {
 			// the activity doesn't implement the interface, throw exception
 			throw new ClassCastException(activity.toString()
-				+ " must implement EnterFileNameSDialogListener");
+					+ " must implement EnterFileNameSDialogListener");
 		}
 	}
-	
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		// Use the current date as the default date in the picker
@@ -40,13 +44,18 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 		int year = c.get(Calendar.YEAR);
 		int month = c.get(Calendar.MONTH);
 		int day = c.get(Calendar.DAY_OF_MONTH);
-		
+
 		// Create a new instance of DatePickerDialog and return it
 		return new DatePickerDialog(getActivity(), this, year, month, day);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.DatePickerDialog.OnDateSetListener#onDateSet(android.widget.DatePicker, int, int, int)
+	 */
 	@Override
-	public void onDateSet(DatePicker view, int year, int month, int day) {
-		// Do something with the date chosen by the user
+	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+		mListener.onDateSelect(DatePickerFragment.this, year, monthOfYear, dayOfMonth);
 	}
+
 }
