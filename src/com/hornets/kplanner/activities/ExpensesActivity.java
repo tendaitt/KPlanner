1adcf750764ad26bfd40a0a4f9407091838cbc5e
Mehmet Kologlu <mehmetk@Mehmets-MacBook-Pro.local>
2013-10-28 21:02:56 -0400

    INCREASED THE API TO 14 to use a switch / improved expenses layout

--- src/com/hornets/kplanner/activities/ExpensesActivity.java
+++ src/com/hornets/kplanner/activities/ExpensesActivity.java
@@ -4,8 +4,6 @@ import java.util.Calendar;
 
 import com.hornets.kplanner.R;
 import com.hornets.kplanner.fragments.DatePickerFragment;
-
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
@@ -22,23 +20,23 @@ import android.widget.Button;
 
 public class ExpensesActivity extends FragmentActivity
 implements DatePickerFragment.DatePickerDialogListener{
-	
+
 	Button dateBtn;
 	int year;
 	int month;
 	int day;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_expenses);
-		
+
 		//get current date
 		final Calendar c = Calendar.getInstance();
 		year = c.get(Calendar.YEAR);
 		month = c.get(Calendar.MONTH);
 		day = c.get(Calendar.DAY_OF_MONTH);
-		
+
 		//define the date button
 		dateBtn = (Button) this.findViewById(R.id.expenses_button_datepicker);
 
@@ -54,12 +52,19 @@ implements DatePickerFragment.DatePickerDialogListener{
 	}
 
 	/*
-	 * onclick function of the pick a date button, 
+	 * onclick of the pick a date button, 
 	 * displays the datepicker fragment
 	 */
 	public void showDatePickerDialog(View v){
 		DatePickerFragment datePickerFragment = new DatePickerFragment();
 		datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+	}
+
+	/*
+	 * onclick of the reminders switch
+	 */
+	public void onSwitchClicked(View v) {
+		
 	}
 
 	/*
