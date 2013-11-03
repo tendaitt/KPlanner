f3ef871340a22a1cb733486efe589f15746d6841
rana-Kzoo <k11rh01@kzoo.edu>
2013-11-02 20:50:46 -0400

    edit activity is working now

--- src/com/hornets/kplanner/activities/EditActivity.java
+++ src/com/hornets/kplanner/activities/EditActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.FrameLayout;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
 import android.widget.TabWidget;
+import android.widget.TextView;
 
 import com.hornets.kplanner.R;
 
@@ -16,14 +19,40 @@ public class EditActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit);
 		
-		TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
-		tabHost.setup();
+		/*final TabWidget tabWidge = tabHost.getTabWidget();
+		final FrameLayout tabContent = tabHost.getTabContentView();*/
 		
-		final TabWidget tabWidge = tabHost.getTabWidget();
-		final FrameLayout tabContent = tabHost.getTabContentView();
-		
-	}
+		final TabHost tabHost=(TabHost)findViewById(R.id.tabhost); 
 
+		tabHost.setup();  
+		TabSpec spec1=tabHost.newTabSpec("Tab1"); 
+		spec1.setContent(R.id.Income);
+		spec1.setIndicator("Tab1",null);  
+		 
+		TabSpec spec2=tabHost.newTabSpec("Tab2"); 
+		spec2.setContent(R.id.Expenses); 
+		spec2.setIndicator("Tab2",null);   
+		
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
+		
+		tabHost.setOnTabChangedListener(new OnTabChangeListener() 
+
+		{
+			@Override
+			public void onTabChanged(String arg0)
+			{ 
+				setTabColor(tabHost); 
+			} 
+		});
+		setTabColor(tabHost);
+	} 
+	public void setTabColor(TabHost tabhost) {
+		for(int i=0;i<tabhost.getTabWidget().getChildCount();i++)
+		{ TextView tv = (TextView) tabhost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+		   
+		}
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
