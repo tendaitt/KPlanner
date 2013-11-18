20ee4c772870c28d4627a244fa210acbbd7a0acd
tendaitt <tendaitt@gmail.com>
2013-11-18 14:03:22 -0500

    added getSummary method for income

--- src/com/hornets/kplanner/dataobjects/OffCampusIncome.java
+++ src/com/hornets/kplanner/dataobjects/OffCampusIncome.java
@@ -2,26 +2,27 @@ package com.hornets.kplanner.dataobjects;
 
 import java.sql.Timestamp;
 
-public class OffCampusIncome extends Income{
+public class OffCampusIncome extends Income {
 
 	private double amount = 0.0;
-	
+
 	public OffCampusIncome(Timestamp date, String rate, String hours) {
 		super(date, rate, hours);
 	}
 
-	public double getAmount(){
-		//get current date
-		//get previous date
-		//check two week schedule
-		//calculate amount
-		
+	public double getAmount() {
+		// get current date
+		// get previous date
+		// check two week schedule
+		// calculate amount
+
 		return amount;
 	}
-	
-@Override
-public String toString(){
-		
-		return "Off Campus Income:"+ '\t'+amount+'\n';
+
+	@Override
+	public String toString() {
+
+		return "Off Campus Income:" + '\t' + amount + '\n';
 	}
+		
 }
