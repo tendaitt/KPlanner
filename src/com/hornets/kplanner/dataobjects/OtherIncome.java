f7e154474a5487e97f473e1f7fa85cda1c5d965f
tendaitt <tendaitt@gmail.com>
2013-11-19 11:04:58 -0500

    changes to db and Income structure

--- src/com/hornets/kplanner/dataobjects/OtherIncome.java
+++ src/com/hornets/kplanner/dataobjects/OtherIncome.java
@@ -1,14 +1,13 @@
 
 package com.hornets.kplanner.dataobjects;
 
-import java.sql.Timestamp;
 
 public class OtherIncome extends Income {
 
 	private double amount = 0.0;
 
-	public OtherIncome(Timestamp date, String rate, String hours) {
-		super(date, rate, hours);
+	public OtherIncome(String date, String rate, String hours,String recurrence) {
+		super(date, rate, hours, recurrence);
 	}
 
 	public double getAmount(){
