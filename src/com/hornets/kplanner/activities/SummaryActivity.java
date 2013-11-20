20da5e85f9459af7b404116300de9f34ca9d4ec2
tendaitt <tendaitt@gmail.com>
2013-11-20 09:29:53 -0500

    exception handling for income in summary

--- src/com/hornets/kplanner/activities/SummaryActivity.java
+++ src/com/hornets/kplanner/activities/SummaryActivity.java
@@ -40,7 +40,12 @@ public class SummaryActivity extends Activity {
 		otherIncomeView = (TextView) findViewById(R.id.summary_text_others);
 		expenseLinearLayout = (LinearLayout) findViewById(R.id.summary_linear_expenselist);
 
-		converter = new DbEntryConverter(getApplicationContext());
+		try{
+			converter = new DbEntryConverter(getApplicationContext());
+		}
+		catch(NullPointerException e){
+			e.printStackTrace();
+		}
 
 		loadIncomeValues();
 		loadExpenseValues();
@@ -48,27 +53,34 @@ public class SummaryActivity extends Activity {
 
 	private void loadIncomeValues() {
 
-		//DEAL WITH NULL RETURNS
-		String onCampus = converter.getOnCampusIncome().toString();
-		String offCampus = converter.getOffCampusIncome().toString();
-		String otherIncome = converter.getOtherIncome().toString();
+		if(!(converter==null)){
+			try{
 
-		onCampusView.setText(onCampus);
-		offCampusView.setText(offCampus);
-		otherIncomeView.setText(otherIncome);
+
+				String onCampus = converter.getOnCampusIncome().toString();
+				String offCampus = converter.getOffCampusIncome().toString();
+				String otherIncome = converter.getOtherIncome().toString();
+				onCampusView.setText(onCampus);
+				offCampusView.setText(offCampus);
+				otherIncomeView.setText(otherIncome);
+			}
+			catch(NullPointerException e){
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private void loadExpenseValues() {
 		expenseArray = converter.getExpenseList();
-		
+
 		int i=0;
-		
+
 		for(Expense e: expenseArray)
 		{
 			//a row
 			LinearLayout row = new LinearLayout(getApplicationContext());
 			row.setOrientation(0);
-			
+
 			//the name of the expense
 			TextView name = new TextView(getApplicationContext());
 			name.setText(e.getName());
@@ -78,14 +90,14 @@ public class SummaryActivity extends Activity {
 			type.setText(e.getDate());
 			TextView amount = new TextView(getApplicationContext());
 			type.setText(e.getAmount());
-			
+
 			row.addView(name);
 			row.addView(type);
 			row.addView(date);
 			row.addView(amount);
-			
+
 			i++;
-			
+
 			expenseLinearLayout.addView(row);			
 		}
 		Toast.makeText(getApplicationContext(),Integer.toString(i),Toast.LENGTH_LONG).show();
