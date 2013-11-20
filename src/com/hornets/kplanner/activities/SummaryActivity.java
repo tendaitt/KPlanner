19bb8aee0426907a35fecd75b3f35208df8a1a46
rana-Kzoo <k11rh01@kzoo.edu>
2013-11-20 12:22:15 -0500

    Edit.java

--- src/com/hornets/kplanner/activities/SummaryActivity.java
+++ src/com/hornets/kplanner/activities/SummaryActivity.java
@@ -55,67 +55,9 @@ public class SummaryActivity extends Activity {
 		return true;
 	}
 	
-	/*public String printDb()
-	{
-	KPlannerSQLHelper dbHelper = new KPlannerSQLHelper(this);
-	SQLiteDatabase dbase = dbHelper.getReadableDatabase();
-	
-	String[] projection = {
-<<<<<<< HEAD
-	KPlannerEntry.COLUMN_INCOME,
-	KPlannerEntry.COLUMN_EXPENSES
-=======
-	KPlannerEntry.COLUMN_USER_NAME,
-	KPlannerEntry.COLUMN_PASSWORD
->>>>>>> a9a3d7a19600476f4eac76e9c1af4862e2c0f40a
-	//KPlannerEntry.Column_saved_data
-	};
- 
-	//how to sort results
-<<<<<<< HEAD
-	// sorting data
-	try {
-		String sortOrder = KPlannerSQLHelper.COLUMN_NAME_CT + " DESC";
 
-		Cursor c = dbase.query(KPlannerSQLHelper.TABLE_NAME, projection, null, null,
-				null, null, sortOrder);
-
-		StringBuilder sb = new StringBuilder();
-		c.moveToFirst();
-		for (String i : c.getColumnNames()) {
-			sb.append(i);
-			sb.append("|");
-		}
-		sb.append("\n");
-
-		while (!c.isAfterLast()) {
-			sb.append(c.getString(0));
-			sb.append("|");
-			for (int i = 1; i < 3; i++) {
-				sb.append(c.getInt(i));
-				sb.append("|");
-			}
-			sb.append(c.getString(3));
-			sb.append("\n");
-			c.moveToNext();
-		}
-		return sb.toString();
-	} catch (Exception e) {
-		return "data is empty";
-	}
-}
-
-public void viewdb(View v) {
+/*public void viewdb(View v) {
 	((TextView) findViewById(R.id.view)).setTextSize(15);
 	((TextView) findViewById(R.id.view)).setText(" " + this.printDb());
-}
-=======
-	try{
-		String sortedOrder="";
-	}
-	
-	
-	}
->>>>>>> a9a3d7a19600476f4eac76e9c1af4862e2c0f40a
-*/
+}*/
 }
