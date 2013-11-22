f46f2ebcced6a0f0ae3b661fc7a63df36c5bec80
Mehmet Kologlu <kologlu.mehmet@gmail.com>
2013-11-21 21:02:20 -0500

    rearranged methods // added comments // dealt with intent from edit activity

--- src/com/hornets/kplanner/activities/ExpensesActivity.java
+++ src/com/hornets/kplanner/activities/ExpensesActivity.java
@@ -1,5 +1,7 @@
 package com.hornets.kplanner.activities;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 import com.hornets.kplanner.R;
@@ -51,55 +53,87 @@ implements DatePickerFragment.DatePickerDialogListener{
 	private Spinner spinnerType;
 
 	private SQLiteDatabase db;
+	private KPlannerSQLHelper dbHelper;
 
 	private Calendar c;
 
-	private int currentYear;
-	private int currentMonth;
-	private int currentDay;
-	
+	private Bundle extras;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_expenses);
 
-		initializeLayoutElements();
+		populateLayout();
 		initializeAndOpenDatabase();
-		initializeCalendar();
-		//update the date picker button to display the date which is the current date
-		updateDateButtonText();
-		
-		Bundle extras= getIntent().getExtras();
-		if(extras != null){
-			edittextName.setText(extras.getString("name"));
-			
-			//FIX THIS
-			spinnerType.setSelection(1);
-			dateBtn.setText(extras.getString("date"));
-			edittextAmount.setText(extras.getString("amount"));
+	}
+
+	/**
+	 * initializes the layout, gets the intent, initializes the calendar, and populates the 
+	 */
+	private void populateLayout() {
+		initializeLayoutElements();
+		setDate();
+		if(extras != null) {
+			populateForEdit();
 		}
 	}
 
-	private void initializeCalendar() {
-		c = Calendar.getInstance();
-		currentYear = c.get(Calendar.YEAR);
-		currentMonth = c.get(Calendar.MONTH)+1; //+1 to compensate for 0 indexing
-		currentDay = c.get(Calendar.DAY_OF_MONTH);
-		resetDate();
-	}
-
-	private void initializeAndOpenDatabase() {
-		KPlannerSQLHelper dbHelper = new KPlannerSQLHelper(getApplicationContext(), "EXPENSES", null, 0);
-		db = dbHelper.getWritableDatabase();
-	}
-
+	/**
+	 * initializes the instances variables as their respective the layout elements
+	 */
 	private void initializeLayoutElements() {
+		//initialize layout elements
 		dateBtn = (Button) findViewById(R.id.expenses_button_datepicker);
 		reminderSwitch = (Switch) findViewById(R.id.expenses_switch_reminder);
 		edittextName = (EditText) findViewById(R.id.expenses_edit_name);
 		edittextAmount = (EditText) findViewById(R.id.expenses_edit_amount);
 		spinnerType = (Spinner) findViewById(R.id.expenses_spinner_type);
+		reminderLinearLayout = (LinearLayout) findViewById(R.id.expneses_linearayout_reminderentry);
+		//get intent
+		extras= getIntent().getExtras();
+	}
+
+	/**
+	 * populates the name, type, and amount fields from the values as passed by the intent
+	 */
+	private void populateForEdit() {
+		//make the name field display the name
+		edittextName.setText(extras.getString("name"));
+		//make the spinner display the type
+		ArrayList<String> typesArray = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.expense_type_array)));
+		spinnerType.setSelection(typesArray.indexOf(extras.getString("type")));
+		//make the amount field display the amount
+		edittextAmount.setText(extras.getString("amount"));
+		//make the date field display the date
+		dateBtn.setText(extras.getString("date"));
+	}
+
+	/**
+	 * get the current date from the java calendar
+	 */
+	private void setDate() {
+		c = Calendar.getInstance();
+		YEAR = c.get(Calendar.YEAR);
+		MONTH = c.get(Calendar.MONTH)+1; //+1 to compensate for 0 indexing
+		DAY = c.get(Calendar.DAY_OF_MONTH);
+		updateDateButtonText();
+	}
+
+	/**
+	 * updates the text of the pick a date button as
+	 */
+	public void updateDateButtonText()
+	{
+		dateBtn.setText(MONTH + "/" + DAY + "/" + YEAR);
+	}
+
+	/**
+	 * initializes a db helper and gets a writable database 
+	 */
+	private void initializeAndOpenDatabase() {
+		dbHelper = new KPlannerSQLHelper(getApplicationContext(), "EXPENSES", null, 0);
+		db = dbHelper.getWritableDatabase();
 	}
 
 	@Override
@@ -127,7 +161,7 @@ implements DatePickerFragment.DatePickerDialogListener{
 		return super.onOptionsItemSelected(item);
 	}
 
-	/*
+	/**
 	 * onclick of the pick a date button, 
 	 * displays the datepicker fragment
 	 */
@@ -136,8 +170,23 @@ implements DatePickerFragment.DatePickerDialogListener{
 		datePickerFragment.show(getSupportFragmentManager(), "datePicker");
 	}
 
+	/**
+	 * (non-Javadoc)
+	 * @see com.hornets.kplanner.fragments.DatePickerFragment.DatePickerDialogListener#onDateSelect(android.support.v4.app.DialogFragment, int, int, int)
+	 * 
+	 * called when the users closes the date picker dialog
+	 * displays the date selected as the text of the date button
+	 * 
+	 */
+	@Override
+	public void onDateSelect(DialogFragment dialog, int y, int monthOfYear, int dayOfMonth) {
+		YEAR = y;
+		MONTH = monthOfYear;
+		DAY = dayOfMonth;
+		updateDateButtonText();
+	}
 
-	/*
+	/**
 	 * onclick of the add button
 	 */
 	public void onClickAdd(View v) {
@@ -167,25 +216,48 @@ implements DatePickerFragment.DatePickerDialogListener{
 		}
 		else //all input fields are filled
 		{
-			//ADD INPUT TO DATABASE
-			//map of values
-			ContentValues values = new ContentValues();
-			values.put(KPlannerEntry.EXPENSE_COLUMN_NAME, name);
-			values.put(KPlannerEntry.EXPENSE_COLUMN_TYPE, type);
-			values.put(KPlannerEntry.EXPENSE_COLUMN_DATE, date);
-			values.put(KPlannerEntry.EXPENSE_COLUMN_AMOUNT, amount);
-
-			//Insert the new row
-			db.insert(KPlannerEntry.EXPENSE_TABLE_NAME, null, values);
+			if(extras != null){
+				editInDatabase(name, type, amount, date);
+			}
+			else
+				addToDatabase(name, type, amount, date);
 
 			//DO SOMETHING WITH THE REMINDER
 
-
-			//toast
+			//notify user
 			Toast.makeText(getApplicationContext(), "Expense added", Toast.LENGTH_SHORT).show();
-			//RESET
+
+			//Reset view
 			resetView();
 		}
+	}
+
+	/**
+	 * removes the entry corresponding to the editIntent's values,
+	 * and adds one with the now selected data
+	 * 
+	 * @param name
+	 * @param type
+	 * @param amount
+	 * @param date
+	 */
+	private void editInDatabase(String name, String type, String amount, String date) {
+		dbHelper.delete(db, extras.getString("name"), extras.getString("type"), extras.getString("date"), extras.getString("amount"));
+		addToDatabase(name, type, amount, date);
+	}
+
+	private void addToDatabase(String name, String type, String amount, String date) {
+		// TODO Auto-generated method stub
+		//map of values
+		ContentValues values = new ContentValues();
+		values.put(KPlannerEntry.EXPENSE_COLUMN_NAME, name);
+		values.put(KPlannerEntry.EXPENSE_COLUMN_TYPE, type);
+		values.put(KPlannerEntry.EXPENSE_COLUMN_DATE, date);
+		values.put(KPlannerEntry.EXPENSE_COLUMN_AMOUNT, amount);
+
+		//Insert the new row
+		db.insert(KPlannerEntry.EXPENSE_TABLE_NAME, null, values);
+
 	}
 
 	/*
@@ -193,74 +265,8 @@ implements DatePickerFragment.DatePickerDialogListener{
 	 */
 	public void onSwitchClicked(View v) {
 
-		reminderLinearLayout = (LinearLayout) findViewById(R.id.expneses_linearayout_reminderentry);
-
-		if(reminderSwitch.isChecked()){
-			//switch is turned on
-
-			//ADD REMINDER INPUT LAYOUT ELEMENTS
-			//define elements
-			final TextView numberEdit = new TextView(this);
-			Button minusButton = new Button(this);
-			RadioGroup reminderRadioGroup = new RadioGroup(this);
-			Button plusButton = new Button(this);
-			final RadioButton daysButton = new RadioButton(this);
-			final RadioButton weeksButton = new RadioButton(this);
-
-			//minus button
-			minusButton.setText(R.string.expenses_button_minus);
-			minusButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v){
-					int current = Integer.parseInt(numberEdit.getText().toString());
-					//decrease by 1
-					if(current != 1){
-						//if the value is not 1, decrease it by 1
-						current--;
-						numberEdit.setText("" + current);
-					}
-					if(current == 1){
-						daysButton.setText(R.string.expeneses_daysButton_radio);
-						weeksButton.setText(R.string.expeneses_weeksButton_radio);
-					}
-				}
-			});
-
-			//the integer
-			numberEdit.setText(R.string.expenses_edit_reminder);
-			numberEdit.setInputType(InputType.TYPE_CLASS_NUMBER);
-
-			//plus button
-			plusButton.setText(R.string.expenses_button_plus);
-			plusButton.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v){
-					//increase by 1
-					int current = Integer.parseInt(numberEdit.getText().toString());
-					if(current == 1)
-					{
-						daysButton.setText("Days");
-						weeksButton.setText("Weeks");
-					}
-					current++;
-					numberEdit.setText("" + current);
-				}
-			});
-
-			//Day & Week Radio Buttons
-			reminderRadioGroup.setOrientation(LinearLayout.HORIZONTAL);
-			reminderRadioGroup.setPadding(0, 10, 0, 0); //padding so it's in the middle
-			daysButton.setText(R.string.expeneses_daysButton_radio);
-			weeksButton.setText(R.string.expeneses_weeksButton_radio);
-			//add radio buttons to the radio group
-			reminderRadioGroup.addView(daysButton);
-			reminderRadioGroup.addView(weeksButton);
-
-			//add all views to the layout
-			reminderLinearLayout.addView(minusButton);
-			reminderLinearLayout.addView(numberEdit);
-			reminderLinearLayout.addView(plusButton);
-			reminderLinearLayout.addView(reminderRadioGroup);
+		if(reminderSwitch.isChecked()){ //switch is turned on
+			populateReminderLayout();
 		}
 		else //switch is turned off
 		{
@@ -268,50 +274,87 @@ implements DatePickerFragment.DatePickerDialogListener{
 		}
 	}
 
-	/*
+	/**
+	 * populates the reminder layout with views to be used to set up a reminder
+	 */
+	private void populateReminderLayout() {
+		// TODO Auto-generated method stub
+		//ADD REMINDER INPUT LAYOUT ELEMENTS
+		//define elements
+		final TextView numberEdit = new TextView(this);
+		Button minusButton = new Button(this);
+		RadioGroup reminderRadioGroup = new RadioGroup(this);
+		Button plusButton = new Button(this);
+		final RadioButton daysButton = new RadioButton(this);
+		final RadioButton weeksButton = new RadioButton(this);
+
+		//minus button
+		minusButton.setText(R.string.expenses_button_minus);
+		minusButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v){
+				int current = Integer.parseInt(numberEdit.getText().toString());
+				//decrease by 1
+				if(current != 1){
+					//if the value is not 1, decrease it by 1
+					current--;
+					numberEdit.setText("" + current);
+				}
+				if(current == 1){
+					daysButton.setText(R.string.expeneses_daysButton_radio);
+					weeksButton.setText(R.string.expeneses_weeksButton_radio);
+				}
+			}
+		});
+
+		//the integer
+		numberEdit.setText(R.string.expenses_edit_reminder);
+		numberEdit.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+		//plus button
+		plusButton.setText(R.string.expenses_button_plus);
+		plusButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v){
+				//increase by 1
+				int current = Integer.parseInt(numberEdit.getText().toString());
+				if(current == 1)
+				{
+					daysButton.setText("Days");
+					weeksButton.setText("Weeks");
+				}
+				current++;
+				numberEdit.setText("" + current);
+			}
+		});
+
+		//Day & Week Radio Buttons
+		reminderRadioGroup.setOrientation(LinearLayout.HORIZONTAL);
+		reminderRadioGroup.setPadding(0, 10, 0, 0); //padding so it's in the middle
+		daysButton.setText(R.string.expeneses_daysButton_radio);
+		weeksButton.setText(R.string.expeneses_weeksButton_radio);
+		//add radio buttons to the radio group
+		reminderRadioGroup.addView(daysButton);
+		reminderRadioGroup.addView(weeksButton);
+
+		//add all views to the layout
+		reminderLinearLayout.addView(minusButton);
+		reminderLinearLayout.addView(numberEdit);
+		reminderLinearLayout.addView(plusButton);
+		reminderLinearLayout.addView(reminderRadioGroup);
+	}
+
+	/**
 	 * onclick of the done button
+	 * closes the database and goes to the homepage
 	 */
 	public void onClickDone(View v) {
 		db.close();
 		NavUtils.navigateUpFromSameTask(this);
 	}
 
-	/*
-	 * updates the text of the pick a date button
-	 */
-	public void updateDateButtonText()
-	{
-		dateBtn.setText(MONTH + "/" + DAY + "/" + YEAR);
-	}
-
-	/*
-	 * sets the text of the pick a date button to the current date
-	 */
-	public void resetDate()
-	{
-		YEAR = currentYear;
-		MONTH = currentMonth;
-		DAY = currentDay;
-		updateDateButtonText();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.hornets.kplanner.fragments.DatePickerFragment.DatePickerDialogListener#onDateSelect(android.support.v4.app.DialogFragment, int, int, int)
-	 * 
-	 * called when the users closes the date picker dialog
-	 * 
-	 */
-	@Override
-	public void onDateSelect(DialogFragment dialog, int y, int monthOfYear, int dayOfMonth) {
-		YEAR = y;
-		MONTH = monthOfYear;
-		DAY = dayOfMonth;
-		updateDateButtonText();
-	}
-
-	/*
-	 * removes all the values entered
+	/**
+	 * removes all the values entered, and resets the data entered to the view
 	 */
 	public void resetView() {
 		//clear the data entered
@@ -320,7 +363,7 @@ implements DatePickerFragment.DatePickerDialogListener{
 		spinnerType.setSelection(0);
 
 		//reset the date
-		resetDate();
+		setDate();
 		//turn the switch off and remove the reminder layout
 		if(reminderSwitch.isChecked() == true){
 			reminderLinearLayout.removeAllViews();
